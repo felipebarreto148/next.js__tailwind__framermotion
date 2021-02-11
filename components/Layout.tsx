bb3de1843361f151ced0984da3aad7bf2a9d717d
Felipe Barreto <felipebarreto148@gmail.com>
2021-02-11 20:34:08 -0300

    Projeto feito a doWhile2020

---
+++ components/Layout.tsx
@@ -0,0 +1,10 @@
+import Header from "./Header"
+
+const Layout: React.FC = ({children}) => {
+    return <div className="max-w-5xl mx-auto px-4 md:px-8 sm:px-6">
+        <Header/>
+        {children}
+    </div>
+
+}
+export default Layout
